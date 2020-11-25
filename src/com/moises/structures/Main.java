cdeaed35ba029c4821066f7be84a2edb6015a365
moisesmtzg <moisesmtzg@gmail.com>
2020-11-25 15:45:22 -0600

    starting dev

---
+++ src/com/moises/structures/Main.java
@@ -0,0 +1,19 @@
+package com.moises.structures;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Arrays myArray = new Arrays();
+		//myArray.problema1();
+		//myArray.problema2();
+		//myArray.problema3();
+		myArray.problema4();
+		
+	}
+
+}
